7fef954ba7c2d4d3d6e5899c64a8f45d2adff8c8
rsskumar <sskumar.reddi@gmail.com>
2018-05-11 21:27:13 -0400

    New Class Addedd

---
+++ Registration.java
@@ -0,0 +1,10 @@
+
+public class Registration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Webdriver driver = new ChromeDriver();
+	//This is Github example
+	}
+
+}
